84d6ea16471f6eca885e920dc2b2ad1cb953bf26
Joel Abshier <jabshier@influxdata.com>
2023-12-06 12:33:46 -0600

    ✨ Day 1 B

--- day1/day1.hs
+++ day1/day1.hs
@@ -1,24 +1,39 @@
 import Control.Monad
 import Data.Char
+import Data.List (findIndex, isPrefixOf)
 import System.IO
 
+-- Constants
 fileName :: String
-fileName = "input.txt"
+fileName = "inputB.txt"
 
+letterWords :: [String]
+letterWords = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
+-- Main
 main = do
   contents <- readFile fileName
-  print . sum . map (combineDigits . getChars) . words $ contents
+  print . sum . map getChars . words $ contents
 
-combineDigits :: (Char, Char) -> Int
-combineDigits (ones, tens) = read [ones, tens]
+-- Helpers
 
-getChars :: String -> (Char, Char)
+combineDigits :: (Int, Int) -> Int
+combineDigits (ones, tens) = ones + tens
+
+getChars :: String -> Int
 getChars str = do
-  let firstChar = first str
-  let lastChar = first (reverse str)
+  let tensPlace = firstB str letterWords
+  let onesPlace = firstB (reverse str) (map reverse letterWords)
 
-  (firstChar, lastChar)
+  (tensPlace * 10) + onesPlace
 
-first :: String -> Char
-first s =
-  if isDigit (head s) then head s else first (tail s)
+firstA :: String -> Int
+firstA s@(h : t) = if isDigit h then read [h] else firstA t
+
+firstB :: String -> [String] -> Int
+firstB s@(h : t) lWords =
+  if isDigit h
+    then read [h]
+    else case findIndex (`isPrefixOf` s) lWords of
+      Nothing -> firstB t lWords
+      (Just i) -> i + 1
